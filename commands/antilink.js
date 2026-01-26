2a3d5e3b99eec174ffafdaaf5f54d0b2dcbbb663
blackmama12 <arunkumar773681@gmail.com>
2026-01-26 19:26:00 +0530

    Create  antilink.js

---
+++ commands/antilink.js
@@ -0,0 +1,9 @@
+import fs from 'fs';
+export default async (sock, msg, args) => {
+    const chat = msg.key.remoteJid;
+    const db = JSON.parse(fs.readFileSync('./media/asura_db.json'));
+    if (!db[chat]) db[chat] = {};
+    db[chat].antilink = (args[0] === 'on');
+    fs.writeFileSync('./media/asura_db.json', JSON.stringify(db, null, 2));
+    await sock.sendMessage(chat, { text: `✅ Antilink is now *${args[0].toUpperCase()}*` });
+};
