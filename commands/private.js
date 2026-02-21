1fbecbbfeb73a067ff08b160ad72c6e3179ff50b
blackmama12 <arunkumar773681@gmail.com>
2026-02-21 15:37:18 +0530

    Update private.js

--- commands/private.js
+++ commands/private.js
@@ -35,7 +35,7 @@ export default async (sock, msg, args) => {
                 title: "ASURA BOT - PRIVATE",
                 body: "Privacy Mode Activated",
                 thumbnail: fs.readFileSync('./media/asura.jpg'),
-                sourceUrl: "", 
+                sourceUrl: "https://whatsapp.com/channel/0029VbB59W9GehENxhoI5l24", 
                 mediaType: 1,
                 renderLargerThumbnail: true
             }
